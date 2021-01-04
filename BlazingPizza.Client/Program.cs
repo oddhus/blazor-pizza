fbd3650003b41cccf2afa9d8c6a443d1ee3f76fc
Oddmund Huseby <huseby.oddmund@gmail.com>
2021-01-04 11:13:09 +0100

    Added custom components

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -18,7 +18,7 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped<OrderState>();
 
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Add auth services
             builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options =>
