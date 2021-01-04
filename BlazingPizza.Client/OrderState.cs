5a7098b5b4bb61919105534ad59def5f7302d745
Oddmund Huseby <odden88@gmail.com>
2021-01-04 08:55:07 +0100

    added auth

--- BlazingPizza.Client/OrderState.cs
+++ BlazingPizza.Client/OrderState.cs
@@ -48,5 +48,10 @@ namespace BlazingPizza.Client
         {
             Order.Pizzas.Remove(pizza);
         }
+
+        public void ReplaceOrder(Order order)
+        {
+            Order = order;
+        }
     }
 }
